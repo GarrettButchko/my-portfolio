476f5bf56c3f4fb74a70972d19878e944664813c
Garrett Butchko <garrettwm2005@gmail.com>
2025-10-28 17:58:53 -0400

    Start EduAndExp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,16 +21,31 @@ export default function Home() {
   return (
     <main className="flex items-top justify-center min-h-screen bg-background">
       <FloatingBar targets={targets} setTargets={setTargets} />
+      <VStack className="mt-24 mb-5 w-full max-w-4xl mx-4" spacing={26}>
+        <Intro/>
+        <EduExp/>
+      </VStack>
+    </main>
+  );
 
-      <VStack className="mt-24 mb-5 w-full max-w-3xl mx-4" spacing={26}>
-        <Section className="bg-foreground rounded-[25px] shadow-lg items-center p-8">
-          <div className="flex sm:flex-row flex-col items-center justify-center gap-5">
-            <VStack className="items-left">
-              <p className="font-bold text-textColor">
-                👋 Hi, I'm
-              </p>
-              <motion.p
-                className="
+  function EduExp() {
+    return (
+      <Section className="bg-foreground rounded-[25px] max-w-4xl shadow-lg items-center py-5">
+        
+      </Section>
+    );
+  }
+
+  function Intro() {
+    return (
+      <Section className="bg-foreground rounded-[25px] max-w-4xl shadow-lg items-center py-5">
+        <div className="flex sm:flex-row flex-col items-center justify-center gap-5">
+          <VStack className="items-left">
+            <p className="font-bold text-textColor">
+              👋 Hi, I'm
+            </p>
+            <motion.p
+              className="
                 md:text-5xl 
                 sm:text-4xl 
                 text-3xl 
@@ -42,35 +57,35 @@ export default function Home() {
                 ease-in-out
                 duration-200
                 text-center"
-                animate={{ backgroundPositionY: ["0%", "100%", "0%"] }}
-                transition={{ duration: 0.2, repeat: Infinity, ease: "easeInOut" }}
-                style={{
-                  backgroundImage: "linear-gradient(to bottom, #0073ffff, #00eeffff)", // blue-400 → teal-300
-                }}
-              >
-                Garrett Butchko
-              </motion.p>
-            </VStack>
-            <Spacer className="hidden sm:block" />
-            <div className="rounded-full">
-              <Image
-                src="/profile.jpeg"     // path from /public
-                alt="My profile picture"
-                width={150}            // required
-                height={150}           // required
-                className="rounded-full border-7 border-oppbackground/10 shadow-lg"
-              />
-            </div>
+              animate={{ backgroundPositionY: ["0%", "100%", "0%"] }}
+              transition={{ duration: 0.2, repeat: Infinity, ease: "easeInOut" }}
+              style={{
+                backgroundImage: "linear-gradient(to bottom, #0073ffff, #00eeffff)", // blue-400 → teal-300
+              }}
+            >
+              Garrett Butchko
+            </motion.p>
+          </VStack>
+          <Spacer className="hidden sm:block" />
+          <div className="rounded-full">
+            <Image
+              src="/profile.jpeg"     // path from /public
+              alt="My profile picture"
+              width={150}            // required
+              height={150}           // required
+              className="rounded-full border-7 border-oppbackground/10 shadow-lg"
+            />
           </div>
+        </div>
 
-          <VStack className="items-center mt-5" spacing={8}>
-            <p className="text-oppbackground/30 text-center">
-              UI/UX Designer | Web & App Developer
-            </p>
-            <div className="flex flex-col sm:flex-row items-center" style={{ gap: `8px` }}>
-              <button
-                type="button"
-                className={`
+        <VStack className="items-center mt-5" spacing={8}>
+          <p className="text-oppbackground/30 text-center">
+            UI/UX Designer | Web & App Developer
+          </p>
+          <div className="flex flex-col sm:flex-row items-center" style={{ gap: `8px` }}>
+            <button
+              type="button"
+              className={`
                   z-20 
                   rounded-[25px]
                   active:scale-95 
@@ -82,9 +97,9 @@ export default function Home() {
                   h-8
                   w-40
                   `}>
-                <Text
-                  variant="body"
-                  className="
+              <Text
+                variant="body"
+                className="
                   justify-center 
                   text-white
                   transition-all
@@ -92,11 +107,27 @@ export default function Home() {
                   duration-300
                   items-center
                   ">
-                  Contact
-                </Text>
+                Contact
+              </Text>
+            </button>
+            <HStack spacing={8}>
+              <button className="
+                  overflow-hidden 
+                  rounded-[5px]
+                  hover:brightness-75
+                  active:scale-95 
+                  transition-all
+                  ease-in-out
+                  duration-300
+                ">
+                <Image
+                  src="/linkedin.jpg"
+                  alt="LinkedIn Picture"
+                  width={32}
+                  height={32}
+                />
               </button>
-              <HStack spacing={8}>
-                <button className="
+              <button className="
                   overflow-hidden 
                   rounded-[5px]
                   hover:brightness-75
@@ -105,34 +136,17 @@ export default function Home() {
                   ease-in-out
                   duration-300
                 ">
-                  <Image
-                    src="/linkedin.jpg"
-                    alt="LinkedIn Picture"
-                    width={32}
-                    height={32}
-                  />
-                </button>
-                <button className="
-                  overflow-hidden 
-                  rounded-[5px]
-                  hover:brightness-75
-                  active:scale-95 
-                  transition-all
-                  ease-in-out
-                  duration-300
-                ">
-                  <Image
-                    src="/github.jpg"     // path from /public
-                    alt="Github Picture"
-                    width={32}
-                    height={32}
-                  />
-                </button>
-              </HStack>
-            </div>
-          </VStack>
-        </Section>
-      </VStack>
-    </main>
-  );
+                <Image
+                  src="/github.jpg"     // path from /public
+                  alt="Github Picture"
+                  width={32}
+                  height={32}
+                />
+              </button>
+            </HStack>
+          </div>
+        </VStack>
+      </Section>
+    );
+  }
 }
